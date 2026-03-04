9d6c0b4f20b154117ec476a09d276eda914957de
nornar0206 <nornar0206@gmail.com>
2026-03-05 01:56:29 +0900

    변수 상수 주석 추가

--- networkstudy/variable_static.cpp
+++ networkstudy/variable_static.cpp
@@ -23,5 +23,9 @@ int main(){
 	
 	std::cout << "num1:" << num1 << std::endl << "num2:" << num2  << std::endl << "num3:" << num3  << std::endl << "num4:" << num4 << std::endl;
 
+	//상수는 입력을 받으려는 시도를 하면 컴파일 단계에서 오류를 발생시킨다.
+	//static은 선언된 위치에 상관없이 프로그램의 시작부터 끝까지 살아있는다.
+	//변수의 자료형으로는 (2byte)(signed)short, unsigned short / (4byte)(signed)int, unsigned int, (signed)long, unsigned long / (8byte) unsigned long long / (4byte) float / (8byte) double, long double / (1byte) (signed)char, unsigned char, bool, string이 있음. 
+
 	return 0;
 }
